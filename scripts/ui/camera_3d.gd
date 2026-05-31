943f1d1e5e048268d8082058bf698885993b4be0
Animatronicat <luckybarsik2710@gmail.com>
2026-05-31 20:17:26 +0300

    Add files via upload

--- scripts/ui/camera_3d.gd
+++ scripts/ui/camera_3d.gd
@@ -8,15 +8,13 @@ extends Camera3D
 @onready var rotate_left: Area2D = $CanvasLayer/rotate_left
 
 @onready var anim_player: AnimationPlayer = $"../AnimationPlayer"
+@onready var rc3d: RayCast3D = $RayCast3D
 
 var pos_main : bool = true
 var first_move = ""
 var second_move = ""
 var is_rotation_finished : bool = true
 func _ready() -> void:
-	#move_to_door.mouse_entered.connect(func() -> void: move("move_to_door"))
-	#move_to_fireplace.mouse_entered.connect(func() -> void: move("move_to_fireplace"))
-	#move_to_couch.mouse_entered.connect(func() -> void: move("move_to_couch"))
 	rotate_right.mouse_entered.connect(func(): rotate_cam(-1))
 	rotate_left.mouse_entered.connect(func(): rotate_cam(1))
 	
@@ -30,49 +28,36 @@ func rotate_cam(dir):
 		tween.tween_property(camera_3d, "rotation_degrees", camera_3d.rotation_degrees + Vector3(0,90,0)*dir, 0.2)
 		tween.tween_callback(func(): is_rotation_finished = true)
 	else:
-		#add to the action memory function in future
+		#add input_buffer in future
 		pass
-		
 
+func move():
+	if rc3d.is_colliding():
+		var target = rc3d.get_collider()
+		if pos_main:
+			pos_main = false
+			anim_player.play(target.name)
+		else:
+			pos_main = true
+			anim_player.play_backwards(target.name)
 
-func move(anim):
-	if pos_main:
-		pos_main = false
-		anim_player.play(anim)
-	else:
-		pos_main = true
-		anim_player.play_backwards(anim)
-
-
-func _on_coll_door_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
+func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.is_pressed():
-			move("move_to_door")
-			if !pos_main:
-				await 1
-				$"../AudioStreamPlayer".play()
-				$"../Sprite3D".show()
-				var tween = get_tree().create_tween()
-				tween.tween_property($"../Sprite3D", "modulate:a", 0, 1.0)
-				tween.tween_callback(abc)
-
-func abc():
-	$"../Sprite3D".hide()
-	$"../Sprite3D".modulate.a = 225
-
-func _on_coll_fireplace_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
-	if event is InputEventMouseButton:
-		if event.is_pressed():
-			move("move_to_fireplace")
-
-
-func _on_couch_coll_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
-	if event is InputEventMouseButton:
-		if event.is_pressed():
-			move("move_to_couch")
-
-
-func _on_table_coll_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
-	if event is InputEventMouseButton:
-		if event.is_pressed():
-			move("move_to_table")
+			move()
+	
+	if Input.is_action_just_pressed("left"):
+		rotate_cam(1)
+	
+	if Input.is_action_just_pressed("right"):
+		rotate_cam(-1)
+	
+	if Input.is_action_just_pressed("forword"):
+		if pos_main:
+			move()
+	
+	if Input.is_action_just_pressed("back"):
+		if !pos_main:
+			move()
+		else:
+			rotate_cam(-2)
